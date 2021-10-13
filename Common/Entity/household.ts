87d31994482136b35b364924b62057f1028a78c6
Joacim Olsson <Joacim.olsson95@gmail.com>
2021-10-13 15:27:31 +0200

    added household in redux

---
+++ Common/Entity/household.ts
@@ -0,0 +1,7 @@
+import task from "./task";
+
+export default interface household {
+  id: string;
+  Tasks: task[];
+  value: number;
+}
--- household-app/Redux/store/rootReducer.ts
+++ household-app/Redux/store/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import householdReducer from "../household/householdReducer";
 import taskReducer from "../task/taskReducer";
 import userReducer from "../user/userReducer";
 
 export const rootReducer = combineReducers({
-    task: taskReducer,
-    user: userReducer,
+  task: taskReducer,
+  user: userReducer,
+  household: householdReducer,
 });
