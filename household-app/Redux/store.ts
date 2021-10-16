41c634868482c7985a983b76cf8b4dc7f2325336
Joacim Olsson <Joacim.olsson95@gmail.com>
2021-10-16 16:09:42 +0200

    add to store and hook

--- household-app/Redux/store.ts
+++ household-app/Redux/store.ts
@@ -1,10 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import loginUserSliceReducer, {
+  loginUserSlice,
+} from "./Service/loginUser/loginUserSlice";
 import { userApi } from "./Service/user/userApi";
 
 export const store = configureStore({
   reducer: {
     [userApi.reducerPath]: userApi.reducer,
+    [loginUserSlice.name]: loginUserSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(userApi.middleware),
 });
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
