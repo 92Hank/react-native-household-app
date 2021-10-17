bc7670a6f0a74c22dc810794eb96600877898c3f
Fredrik Dhami <fredrikdhami@hotmail.com>
2021-10-17 23:52:04 +0200

    Errors fixed. Refactored code.

--- household-app/component/PieChartLabels.tsx
+++ household-app/component/PieChartLabels.tsx
@@ -21,38 +21,39 @@ type LabelProps = {
 
 const PieChartLabels = ({ slices }: LabelProps) => {
 
-
     // https://stackoverflow.com/q/61733861 //typa upp med slice
     // https://github.com/JesperLekland/react-native-svg-charts#arguments-to-children-5
 
     return (
-        slices!.map((slice, index) => {
-            const { labelCentroid, pieCentroid, data } = slice;
-            return (
-                <G
-                    key={index}
-                    x={labelCentroid[0]}
-                    y={labelCentroid[1]}
-                >
-                    <Circle
-                        r={18}
-                        fill={'#fff'}
-                    />
-                    <Image
-                        x={-10}
-                        y={-10}
-                        width={20}
-                        height={20}
-                        preserveAspectRatio="xMidYMid slice"
-                        opacity="1"
-                        href={Assets.images[2]}
-                    />
-                </G>
-            )
-        })
+        <>
+            {
+                slices!.map((slice, index) => {
+                    const { labelCentroid, pieCentroid, data } = slice;
+                    return (
+                        <G
+                            key={index}
+                            x={labelCentroid[0]}
+                            y={labelCentroid[1]}
+                        >
+                            <Circle
+                                r={18}
+                                fill={'#fff'}
+                            />
+                            <Image
+                                x={-10}
+                                y={-10}
+                                width={20}
+                                height={20}
+                                preserveAspectRatio="xMidYMid slice"
+                                opacity="1"
+                                href={Assets.images[2]}
+                            />
+                        </G>
+                    )
+                })
+            }
+        </>
     )
-
-
 };
 
 export default PieChartLabels;
