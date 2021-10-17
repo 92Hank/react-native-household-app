33e2c2f19cfd36fc8d7fd0ae7fe12e1cef05c856
Joacim Olsson <Joacim.olsson95@gmail.com>
2021-10-17 11:39:34 +0200

    change LogIn to go to api

--- household-app/Redux/features/loginUser/loginUserApi.ts
+++ household-app/Redux/features/loginUser/loginUserApi.ts
@@ -1,20 +1,20 @@
 import user from "../../Service/user/User";
 
 // A mock function to mimic making an async request for data
-export function LogIn(userName: string, password: string) {
-  return new Promise<{ data: user }>((resolve) =>
-    setTimeout(
-      () =>
-        resolve({
-          data: {
-            id: "foo",
-            email: "foo",
-            firstName: "foo",
-            isOwner: false,
-            lastName: "foo",
-          },
-        }),
-      500
-    )
-  );
+export function LogIn(email: string, password: string) {
+  return new Promise<{ data: user }>(async (resolve) => {
+    const rawResponse = await fetch(
+      "http://localhost:5000/react-native-household-app/us-central1/webApi/users/signin",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      }
+    );
+    const user: user = await rawResponse.json();
+    resolve({ data: user });
+  });
 }
