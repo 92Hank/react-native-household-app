504abcd66e49ce1595adcf52652193d8209bdb7b
Henke <70940165+92Hank@users.noreply.github.com>
2021-10-18 12:20:47 +0200

    added "header" inside modal and two cards

--- household-app/component/modal/ModalComponent.tsx
+++ household-app/component/modal/ModalComponent.tsx
@@ -9,11 +9,15 @@ import {
   View,
 } from "react-native";
 import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { Card } from "react-native-paper";
+import styles from "./styles";
 
 interface Props {
     isOpen: boolean;
     handleAddClose: () => void;
 }
+
+const recurring = 7;
 
 export default function ModalComponent( props: Props) {
   return (
@@ -26,14 +30,21 @@ export default function ModalComponent( props: Props) {
           props.isOpen;
         }}
       >
-        <View style={styles.centeredView}>
+        <View
+          style={[
+            props.isOpen ? styles.centeredViewBlurred : styles.centeredView,
+          ]}
+        >
           <View style={styles.modalView}>
-            <Text style={styles.modalText}>Create a Task</Text>
+            <View style={styles.modalTextView}>
+              <Text style={styles.modalText}>Skapa en ny syssla</Text>
+            </View>
+
             <View style={styles.textinputTitleRight}>
               <TextInput
                 style={styles.titleContent}
                 multiline={true}
-                placeholder="Title.."
+                placeholder="Titel.."
               ></TextInput>
             </View>
             <View style={styles.container}>
@@ -41,17 +52,37 @@ export default function ModalComponent( props: Props) {
                 <TextInput
                   style={styles.taskItemContent}
                   multiline={true}
-                  placeholder="Description.."
+                  placeholder="Beskrivning.."
                 ></TextInput>
               </View>
             </View>
 
-            <TouchableHighlight
+            {/* <TouchableHighlight
               style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
               onPress={props.handleAddClose}
             >
               <Text style={styles.textStyle}>Hide Modal</Text>
-            </TouchableHighlight>
+            </TouchableHighlight> */}
+
+            <Card style={styles.inputsCard}>
+              <Card.Content>
+                <Text>
+                  <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                    Återkommer:
+                  </Text>
+                  <Text style={{ fontSize: 16 }}> var {recurring} dag</Text>
+                </Text>
+              </Card.Content>
+            </Card>
+            <Card style={styles.inputsCard2}>
+              <Card.Title
+                title="Värde: "
+                subtitle="Hur energikrävande är sysslan?"
+              />
+              <Card.Content>
+                <Text style={{ fontSize: 16 }}> 1</Text>
+              </Card.Content>
+            </Card>
 
             <View style={styles.buttonsContainer}>
               <TouchableOpacity
@@ -63,7 +94,7 @@ export default function ModalComponent( props: Props) {
                   size={30}
                   color="black"
                 />
-                <Text style={styles.householdButtonText}>Save</Text>
+                <Text style={styles.householdButtonText}>Spara</Text>
               </TouchableOpacity>
               <TouchableOpacity
                 onPress={props.handleAddClose}
@@ -74,7 +105,7 @@ export default function ModalComponent( props: Props) {
                   size={30}
                   color="black"
                 />
-                <Text style={styles.householdButtonText}>Close</Text>
+                <Text style={styles.householdButtonText}>Stäng</Text>
               </TouchableOpacity>
             </View>
           </View>
@@ -83,151 +114,3 @@ export default function ModalComponent( props: Props) {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 0,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    height: "80%",
-    width: "95%",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  openButton: {
-    backgroundColor: "#F194FF",
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  container: {
-    marginTop: 15,
-  },
-  textinputContainerRight: {
-    flex: 1,
-    alignContent: "flex-start",
-    marginLeft: 15,
-    marginTop: 0,
-    marginRight: 15,
-    marginBottom: 0,
-    backgroundColor: "#ffffff",
-    maxHeight: 160,
-    minWidth: 360,
-    borderRadius: 8,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 0,
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 12.35,
-    elevation: 10,
-  },
-  taskItemContent: {
-    color: "black",
-    fontSize: 20,
-    marginLeft: 10,
-    marginTop: 10,
-  },
-  textinputTitleRight: {
-    flex: 1,
-    justifyContent: "center",
-    alignContent: "flex-end",
-    marginLeft: 15,
-    marginTop: 0,
-    marginRight: 15,
-    marginBottom: 0,
-    backgroundColor: "#ffffff",
-    maxHeight: 60,
-    minWidth: 360,
-    borderRadius: 8,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 0,
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 12.35,
-    elevation: 10,
-  },
-  titleContent: {
-    color: "black",
-    fontSize: 20,
-    marginLeft: 10,
-  },
-  buttonsContainer: {
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignSelf: "flex-end",
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    marginRight: 10,
-    marginLeft: 10,
-  },
-  closeButton: {
-    backgroundColor: "white",
-    paddingVertical: 20,
-    paddingHorizontal: 20,
-    width: 195,
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "center",
-    shadowColor: "rgba(0, 0, 0, 0.1)",
-    shadowOpacity: 0.8,
-    elevation: 6,
-    shadowRadius: 15,
-    shadowOffset: { width: 1, height: 13 },
-    borderBottomRightRadius: 20,
-    borderStartWidth: 1,
-    borderStartColor: 'gainsboro'
-  },
-  saveButton: {
-    backgroundColor: "white",
-    paddingVertical: 20,
-    paddingHorizontal: 20,
-    width: 195,
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "center",
-    shadowColor: "rgba(0, 0, 0, 0.1)",
-    shadowOpacity: 0.8,
-    elevation: 6,
-    shadowRadius: 15,
-    shadowOffset: { width: 1, height: 13 },
-    borderBottomLeftRadius: 20,
-  },
-  householdButtonText: {
-    color: "black",
-    fontSize: 18,
-    fontWeight: "bold",
-    marginLeft: 15,
-  },
-});
