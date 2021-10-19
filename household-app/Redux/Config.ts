b78b613746210f4cb81b99fb52c4d71de70becd3
capslockrobin <71007168+capslockrobin@users.noreply.github.com>
2021-10-19 16:03:54 +0200

    fix readMe

--- household-app/Redux/Config.ts
+++ household-app/Redux/Config.ts
@@ -1,4 +1,4 @@
 
-export const LocalIp = "http://172.19.200.32:5001";
+export const LocalIp = "192.168.0.23:5001";
 
 export const webUrl = `http://${LocalIp}/react-native-household-app/us-central1/webApi/`;
