e2aecdd937bdddbd82ea748968f9d0883b0e7b9e
Joacim Olsson <Joacim.olsson95@gmail.com>
2021-10-21 12:00:33 +0200

    selectSelectedTask

--- household-app/Redux/features/SelectedState/SelectedStateSelectors.ts
+++ household-app/Redux/features/SelectedState/SelectedStateSelectors.ts
@@ -2,3 +2,6 @@ import { RootState } from "../../store";
 
 export const selectSelectedHousehold = (state: RootState) =>
   state.SelectedState.SelectedHousehold;
+
+export const selectSelectedTask = (state: RootState) =>
+  state.SelectedState.SelectedTask;
