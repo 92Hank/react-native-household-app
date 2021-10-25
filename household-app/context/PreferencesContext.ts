d38c56d7a08f96a41565bab12a0082b353ee3ad1
Joacim Olsson <Joacim.olsson95@gmail.com>
2021-10-25 09:29:45 +0200

    fix "," on .json and make a disable on PreferencesContext

--- household-app/context/PreferencesContext.ts
+++ household-app/context/PreferencesContext.ts
@@ -7,5 +7,6 @@ type PreferencesContextType = {
 
 export const PreferencesContext = React.createContext<PreferencesContextType>({
     theme: "light",
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     toggleTheme: () => {},
 });
