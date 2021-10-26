f549c7879992d6a19d9581de82207a41a2061178
Fredrik Dhami <fredrikdhami@hotmail.com>
2021-10-26 19:04:43 +0200

    Helper method exported.

--- household-app/screens/Tasks/doneTaskHelper.ts
+++ household-app/screens/Tasks/doneTaskHelper.ts
@@ -1,27 +1,19 @@
 import { doneTask } from "../../../Common/doneTask";
 import { household } from "../../../Common/household";
 
-export const filterDoneTasksByHouseHold = (doneTasksArray: doneTask[], currentHousehold: household) => {
+export const getLastMonthDoneTasksByHousehold = (doneTasksArray: doneTask[], currentHousehold: household) => {
+    const filteredArray = filterDoneTasksByHouseHold(doneTasksArray, currentHousehold);
+    const startTime = getLastMonthStartInSeconds();
+    const endTime = getLastMonthEndInSeconds();
+
+    return filterDoneTasksBySecondsInterval(filteredArray, startTime, endTime);
+};
+
+const filterDoneTasksByHouseHold = (doneTasksArray: doneTask[], currentHousehold: household) => {
     return doneTasksArray.filter((doneTask) => doneTask.householdId === currentHousehold.id);
 };
 
-export const getLastMonthStartInSeconds = () => {
-    const lastMonthStartDate = new Date();
-    lastMonthStartDate.setMonth(lastMonthStartDate.getMonth() - 1);
-    lastMonthStartDate.setDate(1);
-    lastMonthStartDate.setHours(0, 0, 0, 0);
-    return Math.floor(lastMonthStartDate.getTime() / 1000);
-};
-
-export const getLastMonthEndInSeconds = () => {
-    const lastMonthEndDate = new Date();
-    lastMonthEndDate.setMonth(lastMonthEndDate.getMonth());
-    lastMonthEndDate.setDate(0);
-    lastMonthEndDate.setHours(23, 59, 59, 999);
-    return Math.floor(lastMonthEndDate.getTime() / 1000);
-};
-
-export const filterDoneTasksBySecondsInterval = (
+const filterDoneTasksBySecondsInterval = (
     doneTasksArray: doneTask[],
     startDateInSeconds: number,
     endDateInSeconds: number,
@@ -31,4 +23,20 @@ export const filterDoneTasksBySecondsInterval = (
 
         if (dateDoneInSeconds - endDateInSeconds <= 0 && dateDoneInSeconds - startDateInSeconds >= 0) return doneTask;
     });
+};
+
+const getLastMonthStartInSeconds = () => {
+    const lastMonthStartDate = new Date();
+    lastMonthStartDate.setMonth(lastMonthStartDate.getMonth() - 1);
+    lastMonthStartDate.setDate(1);
+    lastMonthStartDate.setHours(0, 0, 0, 0);
+    return Math.floor(lastMonthStartDate.getTime() / 1000);
+};
+
+const getLastMonthEndInSeconds = () => {
+    const lastMonthEndDate = new Date();
+    lastMonthEndDate.setMonth(lastMonthEndDate.getMonth());
+    lastMonthEndDate.setDate(0);
+    lastMonthEndDate.setHours(23, 59, 59, 999);
+    return Math.floor(lastMonthEndDate.getTime() / 1000);
 };
