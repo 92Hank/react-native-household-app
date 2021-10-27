485712a685d7d422a7c32a5b22abc6345ca4e938
Henke <70940165+92Hank@users.noreply.github.com>
2021-10-27 12:01:04 +0200

    eslint fixed

--- household-app/component/householdComponents/taskModal/editTaskModal.tsx
+++ household-app/component/householdComponents/taskModal/editTaskModal.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const editTaskModal = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+    return <div></div>;
+};
 
-export default editTaskModal
+export default editTaskModal;
