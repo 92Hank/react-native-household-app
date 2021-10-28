8445c6f0926679b0847ca7cf448a416449650bd0
Fredrik Dhami <fredrikdhami@hotmail.com>
2021-10-29 00:47:13 +0200

    Checks for taskDone.value, attempts to fill in.

--- household-app/screens/Tasks/LastMonthScreen.tsx
+++ household-app/screens/Tasks/LastMonthScreen.tsx
@@ -3,11 +3,12 @@
 
 import React, { FC } from "react";
 import { SafeAreaView, ScrollView, StyleSheet, Text } from "react-native";
+import { doneTask } from "../../../Common/doneTask";
 import StatisticsCharts from "../../component/StatisticsCharts";
-import { selectCurrentLoginUser } from "../../Redux/features/loginUser/LoginSelectors";
 import { selectSelectedHousehold } from "../../Redux/features/SelectedState/SelectedStateSelectors";
 import { useAppSelector } from "../../Redux/hooks";
 import { useGetDoneTasksWithHouseholdIdQuery } from "../../Redux/Service/doneTask/doneTaskApi";
+import { useGetTaskByHouseholdIdQuery } from "../../Redux/Service/task/taskApi";
 import { FeedStackScreenProps, MainRoutes } from "../../routes/routes";
 import { getLastMonthDoneTasksByHousehold } from "./doneTaskHelper";
 import { createMemberStatistics, MemberStatistics } from "./MemberStatistics";
@@ -15,85 +16,43 @@ import { createMemberStatistics, MemberStatistics } from "./MemberStatistics";
 type Props = FeedStackScreenProps<MainRoutes.ProfileScreen>;
 
 const LastMonthScreen: FC<Props> = ({ navigation }: Props): React.ReactElement => {
-    console.log("------------------------------- NEW RENDITION"); //TEST
     const currentHousehold = useAppSelector(selectSelectedHousehold);
-    const currentuser = useAppSelector(selectCurrentLoginUser); //TEST
-    console.log("HOUSEHOLD ID ÄR:   " + currentHousehold!.id!); //TEST
-    // console.log("user ID ÄR:   " + currentuser!.id!); //TEST
+    const { data: doneTasksArray, error: doneTaskError } = useGetDoneTasksWithHouseholdIdQuery(currentHousehold?.id!);
+    const { data: tasksDataArray, error: taskError } = useGetTaskByHouseholdIdQuery(currentHousehold?.id!);
 
-    const { data: doneTasksArray, isSuccess, error } = useGetDoneTasksWithHouseholdIdQuery(currentHousehold?.id!);
+    const haveAllDoneTasksValue = (doneTasksArray: doneTask[]) => {
+        let returnValue = true;
 
-    // console.log("LÄNGD AV DONETASK ARRAY:  " + doneTasksArray!.length) //TEST
+        doneTasksArray.forEach((doneTask) => {
+            console.log("CHECK FOR DONETASK, VALUE" + doneTask.value) //TEST
+            if (!doneTask.value || doneTask.value === undefined) returnValue = false;
+        });
+        return returnValue;
+    };
 
     let statisticsArray: MemberStatistics[] | undefined = undefined;
     let fillerMessage = "No done tasks found for this household.";
 
     if (doneTasksArray !== undefined && doneTasksArray.length > 0 && currentHousehold !== undefined) {
-        const doneTasksOfLastMonth = getLastMonthDoneTasksByHousehold(doneTasksArray, currentHousehold);
+        let doneTasksOfLastMonth = getLastMonthDoneTasksByHousehold(doneTasksArray, currentHousehold);
+
+        console.log("ALLA ODNETASKS OF LAST MONTH")
+        console.log(doneTasksOfLastMonth)
+
+        // if (!haveAllDoneTasksValue(doneTasksOfLastMonth)) {
+        //     console.log("---------------------------------DATAs VALUE check") //TEST
+        //     doneTasksOfLastMonth = fillAllMissingTaskValues(doneTasksOfLastMonth, tasksDataArray!);
+        // }
 
         if (doneTasksOfLastMonth.length === 0) {
             fillerMessage = "No data found for the selected period.";
         } else {
-            console.log("check, doneTaskOfLastMonth array length: " + doneTasksOfLastMonth.length)//TEST
+            console.log("check, doneTaskOfLastMonth array length: " + doneTasksOfLastMonth.length); //TEST
             statisticsArray = createMemberStatistics(doneTasksOfLastMonth, currentHousehold);
-            console.log("statisticsArray after creation run: " + statisticsArray.length) //TEST
-            console.log("statistic 0 doneTask length after creation run: " + statisticsArray[0].doneTasks.length) //TEST
-            console.log("statistic 1 doneTask length after creation run: " + statisticsArray[1].doneTasks.length) //TEST
+            console.log("statisticsArray after creation run: " + statisticsArray.length); //TEST
         }
     }
 
-    //TEST
-    let indexI = 0;
-    statisticsArray?.forEach((member) => {
-        console.log("member" + indexI++ + "doneTask length:   ___ " + member.doneTasks.length)
-    });
-
-    // const dataArray: MemberStatistics[] = [
-    //     {
-    //         //DETTA ÄR EN USER
-    //         key: 1,
-    //         userId: "1",
-    //         emoji: "🐙",
-    //         doneTasks: [testTasksDone[0]], //0
-    //         svg: {
-    //             fill: "#600080",
-    //             onPress: () => console.log("USER1"),
-    //         },
-    //     },
-    //     {
-    //         //DETTA ÄR EN USER
-    //         key: 2,
-    //         userId: "2",
-    //         emoji: "🦊",
-    //         doneTasks: [testTasksDone[1], testTasksDone[2], testTasksDone[4]], //2+2=4
-    //         svg: {
-    //             fill: "green",
-    //             onPress: () => console.log("USER2"),
-    //         },
-    //     },
-    //     {
-    //         //DETTA ÄR EN USER
-    //         key: 3,
-    //         userId: "3",
-    //         emoji: "🐸",
-    //         doneTasks: [testTasksDone[1], testTasksDone[2]], //2+2=4
-    //         svg: {
-    //             fill: "blue",
-    //             onPress: () => console.log("USER3"),
-    //         },
-    //     },
-    //     {
-    //         //DETTA ÄR EN USER
-    //         key: 4,
-    //         userId: "999", //4
-    //         emoji: "🦄",
-    //         doneTasks: [testTasksDone[3], testTasksDone[4]],
-    //         svg: {
-    //             fill: "red",
-    //             onPress: () => console.log("USER4"),
-    //         },
-    //     },
-    // ];
     return (
         <SafeAreaView>
             <ScrollView>
