674aeaca93608c1161eee16ca6ea01f33264baea
Tina Johansson <91chrjoh@skola.boras.se>
2021-11-01 22:31:06 +0100

    Some minor changes to the profile under household members, nothing major

--- household-app/screens/Household/HouseholdProfile.tsx
+++ household-app/screens/Household/HouseholdProfile.tsx
@@ -3,6 +3,7 @@ import { Surface } from "react-native-paper";
 import Button from "../../component/common/Button";
 import ProfileModule from "../../component/profile/ProfileModule";
 import { FeedStackScreenProps, MainRoutes } from "../../routes/routes";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
 type Props = FeedStackScreenProps<MainRoutes.ProfileScreen>;
 
@@ -16,11 +17,25 @@ const HouseholdProfile: FC<Props> = (): React.ReactElement => {
     };
 
     return (
-        <Surface>
-            <Button text="Öppna" onPress={handleTaskOpen} iconType={{ type: "MaterialIcons", icons: "open-in-new" }} />
+        <Surface style={styles.profileSurface}>
+            <Button text="Robin" onPress={handleTaskOpen} 
+            iconType={{ type: "MaterialIcons", icons: "open-in-new" }} 
+            buttonStyle={styles.button} />
             <ProfileModule isOpen={isClickedTaskOpen} handleModalClose={handleTaskClose} />
         </Surface>
     );
 };
+
+const styles = StyleSheet.create({
+    profileSurface: {
+        fontSize: 45,
+        flexWrap: "wrap",
+        flex: 1,
+    },
+    button: {
+        marginVertical: 16, 
+        width: "95%",
+    },
+});
 
 export default HouseholdProfile;
