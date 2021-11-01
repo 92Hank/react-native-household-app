ad8aef302be9c0dca74b99f02863cf39d868cfa0
Fredrik Dhami <fredrikdhami@hotmail.com>
2021-11-01 09:33:38 +0100

    CurrentWeekScreen implemented.

--- household-app/screens/Tasks/CurrentWeekScreen.tsx
+++ household-app/screens/Tasks/CurrentWeekScreen.tsx
@@ -1,14 +1,46 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
 import React, { FC } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import StatisticsCharts from "../../component/piecharts/StatisticsCharts";
+import { selectSelectedHousehold } from "../../Redux/features/SelectedState/SelectedStateSelectors";
+import { useAppSelector } from "../../Redux/hooks";
+import { useGetDoneTasksWithHouseholdIdQuery } from "../../Redux/Service/doneTask/doneTaskApi";
 import { FeedStackScreenProps, MainRoutes } from "../../routes/routes";
+import { getCalendarWeekDoneTasksByHousehold } from "./doneTaskHelper";
+import { createMemberStatistics, MemberStatistics } from "./MemberStatistics";
 
 type Props = FeedStackScreenProps<MainRoutes.ProfileScreen>;
 
 const CurrentWeekScreen: FC<Props> = ({ navigation }: Props): React.ReactElement => {
+    const currentHousehold = useAppSelector(selectSelectedHousehold);
+    const { data: doneTasksArray, error: doneTaskError } = useGetDoneTasksWithHouseholdIdQuery(currentHousehold?.id!);
+
+    let statisticsArray: MemberStatistics[] | undefined = undefined;
+    let fillerMessage = "No done tasks found for this household.";
+
+    if (doneTasksArray !== undefined && doneTasksArray.length > 0 && currentHousehold !== undefined) {
+        const doneTasksOfLastWeek = getCalendarWeekDoneTasksByHousehold(doneTasksArray, currentHousehold, 0);
+        if (doneTasksOfLastWeek.length === 0) {
+            fillerMessage = "No data found for the selected period.";
+        } else {
+            statisticsArray = createMemberStatistics(doneTasksOfLastWeek, currentHousehold);
+        }
+    }
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Nuvarande vecka</Text>
-        </View>
+        <SafeAreaView>
+            <ScrollView>
+                {statisticsArray && currentHousehold ? (
+                    <StatisticsCharts data={statisticsArray} currentHousehold={currentHousehold} />
+                ) : (
+                    <View style={styles.container}>
+                        <Text style={styles.text}>{fillerMessage}</Text>
+                    </View>
+                )}
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
