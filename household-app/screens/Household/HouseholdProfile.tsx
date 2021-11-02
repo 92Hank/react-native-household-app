cc1a0a9a84d0e5ccdea05324a29729e3ef999c2a
Tina Johansson <91chrjoh@skola.boras.se>
2021-11-02 13:56:20 +0100

    profile layout almost done, fixed edit button and member name placement

--- household-app/screens/Household/HouseholdProfile.tsx
+++ household-app/screens/Household/HouseholdProfile.tsx
@@ -50,34 +50,26 @@ const HouseholdProfile: FC<Props> = (): React.ReactElement => {
     return (
         <>
             <View style={styles.topBar}>
-                <View style={styles.leftSide}></View>
                 <View style={styles.avatarImage}>
                     <Text style={styles.avatar}> {Avatars[avatar]} </Text>
-                    {/* <Avatar.Image
-                        size={120}
-                        source={require("../../assets/logotypeBlack/logoBS.png")}
-                        style={styles.profileImage}
-                    /> */}
-                </View>
-                <View style={styles.darkThemeButton}>
-                    <ToggleDarkThemeSwitch>DarkMode Switch</ToggleDarkThemeSwitch>
                 </View>
             </View>
             <Surface style={styles.container}>
                 <Text style={styles.labelText}>Member User Name</Text>
-                <Text style={styles.text}>{username}</Text>
-                <Divider style={styles.divider} />
+                <Surface style={styles.profileSurface}>
+                    <Text style={styles.text}>{username}</Text>
+                    <View style={styles.buttonAlign}>
+                        <Button
+                            text=""
+                            onPress={handleTaskOpen}
+                            iconType={{ type: "MaterialIcons", icons: "edit" }}
+                            buttonStyle={styles.button}
+                        />
+                    </View>
+                    <ProfileModule isOpen={isClickedTaskOpen} handleModalClose={handleTaskClose} />
+                </Surface>
             </Surface>
 
-            <Surface style={styles.profileSurface}>
-                <Button
-                    text="Ändra Profil"
-                    onPress={handleTaskOpen}
-                    iconType={{ type: "MaterialIcons", icons: "open-in-new" }}
-                    buttonStyle={styles.button}
-                />
-                <ProfileModule isOpen={isClickedTaskOpen} handleModalClose={handleTaskClose} />
-            </Surface>
         </>
     );
 };
@@ -85,12 +77,25 @@ const HouseholdProfile: FC<Props> = (): React.ReactElement => {
 const styles = StyleSheet.create({
     profileSurface: {
         fontSize: 45,
-        flexWrap: "wrap",
         flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "flex-start",
+        marginHorizontal: 16,
     },
     button: {
+        width: 60,
+        height: 20,
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: 16,
+    },
+    buttonAlign: {
+        alignItems: "flex-end",
+        justifyContent: "flex-start",
+        flexWrap: "wrap",
         marginVertical: 16,
-        width: "95%",
+        marginLeft: 200,
     },
     topBar: {
         flexDirection: "row",
@@ -98,61 +103,38 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         paddingVertical: 24,
     },
-    leftSide: {
-        flex: 1,
-        alignItems: "flex-start",
-        justifyContent: "flex-start",
-        marginLeft: 16,
-    },
     avatarImage: {
-        justifyContent: "center",
-        alignItems: "center",
-        flex: 3,
-    },
-    profileImage: {
-        backgroundColor: "white",
-        resizeMode: "contain",
+        
     },
     darkThemeButton: {
         paddingRight: 24,
         justifyContent: "flex-end",
     },
-    preference: {
-        alignItems: "flex-end",
-        flexDirection: "row",
-    },
-    themeText: {
-        color: "grey",
-        lineHeight: 50,
-    },
     container: {
         flex: 1,
         marginTop: 20,
     },
-    content: {
-        alignItems: "flex-start",
-        justifyContent: "flex-start",
-        marginHorizontal: 16,
-    },
     labelText: {
         color: "grey",
         fontSize: 16,
+        marginHorizontal: 16,
     },
     text: {
         color: "grey",
         fontSize: 24,
-        marginBottom: 16,
-    },
-    divider: {
-        color: "grey",
-        maxWidth: "100%",
+        marginHorizontal: 16,
+        lineHeight: 50,
     },
     avatar: {
-        fontSize: 45,
+        fontSize: 80,
         margin: 10,
-        flexWrap: "wrap",
-        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
     },
 });
 
 export default HouseholdProfile;
+
+/*<View style={styles.darkThemeButton}>
+    <ToggleDarkThemeSwitch>DarkMode Switch</ToggleDarkThemeSwitch>
+</View>*/
