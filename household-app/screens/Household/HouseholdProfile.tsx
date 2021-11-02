60cf0f8a128f68eb3cd891917c745ad87f03e1c3
Tina Johansson <91chrjoh@skola.boras.se>
2021-11-02 09:19:14 +0100

    - added everything from profilepage to household profile

--- household-app/screens/Household/HouseholdProfile.tsx
+++ household-app/screens/Household/HouseholdProfile.tsx
@@ -1,9 +1,11 @@
 import React, { FC, useState } from "react";
-import { Surface } from "react-native-paper";
 import Button from "../../component/common/Button";
 import ProfileModule from "../../component/profile/ProfileModule";
 import { FeedStackScreenProps, MainRoutes } from "../../routes/routes";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+import { View, Text, StyleSheet } from "react-native";
+import { Avatar, Card, Divider, Surface, Switch, TouchableRipple } from "react-native-paper";
+import { PreferencesContext } from "../../context/PreferencesContext";
 
 type Props = FeedStackScreenProps<MainRoutes.ProfileScreen>;
 
@@ -15,14 +17,66 @@ const HouseholdProfile: FC<Props> = (): React.ReactElement => {
     const handleTaskOpen = () => {
         setIsClickedTaskOpen(true);
     };
+    const { theme, toggleTheme } = React.useContext(PreferencesContext);
+
+    const [isEnabled, setIsEnabled] = useState(false);
+    const [switchValue, setSwitchValue] = useState(false);
+
+    //To handle switch toggle
+    const toggleSwitch = () => {
+        setSwitchValue(true);
+    };
 
     return (
+        <>
+            <View style={styles.topBar}>
+                <View style={styles.leftSide}>
+                    
+                </View>
+                <View style={styles.avatarImage}>
+                    <Avatar.Image
+                        size={120}
+                        source={require("../../assets/logotypeBlack/logoBS.png")}
+                        style={styles.profileImage}
+                    />
+                </View>
+                <View style={styles.darkThemeButton}>
+                    <TouchableRipple onPress={toggleTheme} hasTVPreferredFocus={false} tvParallaxProperties={{}}>
+                        <View style={styles.preference}>
+                            <Text style={styles.themeText}>{theme === "dark" ? "☀️" : "🌙"}</Text>
+                            <View pointerEvents="none">
+                                <Switch
+                                    ios_backgroundColor="salmon"
+                                    value={theme === "dark"}
+                                    trackColor={{ false: "grey", true: "white" }}
+                                    thumbColor={theme === "dark" ? "grey" : "white"}
+                                    onValueChange={toggleSwitch}
+                                />
+                            </View>
+                        </View>
+                    </TouchableRipple>
+                </View>
+            </View>
+            <Surface style={styles.container}>
+                <Text style={styles.labelText}>User Name</Text>
+                <Text style={styles.text}>Lilo24</Text>
+                <Divider style={styles.divider} />
+
+                <Text style={styles.labelText}>Nick Name</Text>
+                <Text style={styles.text}>StitchGoesCrazy</Text>
+                <Divider style={styles.divider} />
+
+                <Text style={styles.labelText}>Password</Text>
+                <Text style={styles.text}>***********</Text>
+                <Divider style={styles.divider} />
+            </Surface>
         <Surface style={styles.profileSurface}>
             <Button text="Robin" onPress={handleTaskOpen} 
             iconType={{ type: "MaterialIcons", icons: "open-in-new" }} 
             buttonStyle={styles.button} />
             <ProfileModule isOpen={isClickedTaskOpen} handleModalClose={handleTaskClose} />
         </Surface>
+        </>
     );
 };
 
@@ -35,6 +89,61 @@ const styles = StyleSheet.create({
     button: {
         marginVertical: 16, 
         width: "95%",
+    },
+    topBar: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingVertical: 24,
+    },
+    leftSide: {
+        flex: 1,
+        alignItems: "flex-start",
+        justifyContent: "flex-start",
+        marginLeft: 16,
+    },
+    avatarImage: {
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 3,
+    },
+    profileImage: {
+        backgroundColor: "white",
+        resizeMode: "contain",
+    },
+    darkThemeButton: {
+        paddingRight: 24,
+        justifyContent: "flex-end",
+    },
+    preference: {
+        alignItems: "flex-end",
+        flexDirection: "row",
+    },
+    themeText: {
+        color: "grey",
+        lineHeight: 50,
+    },
+    container: {
+        flex: 1,
+        marginTop: 20,
+    },
+    content: {
+        alignItems: "flex-start",
+        justifyContent: "flex-start",
+        marginHorizontal: 16,
+    },
+    labelText: {
+        color: "grey",
+        fontSize: 16,
+    },
+    text: {
+        color: "grey",
+        fontSize: 24,
+        marginBottom: 16,
+    },
+    divider: {
+        color: "grey",
+        maxWidth: "100%",
     },
 });
 
